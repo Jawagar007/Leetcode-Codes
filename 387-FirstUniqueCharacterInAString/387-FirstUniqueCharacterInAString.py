56e7fae8490235057d5b69dcd34e54e282cd6139
Jawagar007 <jawagargandhi01@gmail.com>
2026-02-04 22:44:44 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.py
@@ -0,0 +1,11 @@
+# Last updated: 04/02/2026, 22:44:43
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq = {}
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+        for i in range(len(s)):
+            if freq[s[i]] == 1:
+                return i
+        return -1
+       
