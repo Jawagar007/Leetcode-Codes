7e8b1a7650a9276d80ef6d4c53a7d3410f695e2b
Jawagar007 <jawagargandhi01@gmail.com>
2026-02-04 22:44:20 +0530

    Create 2542-AverageValueOfEvenNumbersThatAreDivisibleByThree

---
+++ 2542-AverageValueOfEvenNumbersThatAreDivisibleByThree/2542-AverageValueOfEvenNumbersThatAreDivisibleByThree.py
@@ -0,0 +1,10 @@
+# Last updated: 04/02/2026, 22:44:18
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        total = 0
+        count = 0
+        for num in nums:
+            if num % 2 == 0 and num % 3 == 0:
+                total += num
+                count += 1
+        return total // count if count > 0 else 0
