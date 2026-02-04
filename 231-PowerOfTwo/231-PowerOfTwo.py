4c367b57864b538874c298162f30b0ee0059a278
Jawagar007 <jawagargandhi01@gmail.com>
2026-02-04 22:44:50 +0530

    Create 231-PowerOfTwo

---
+++ 231-PowerOfTwo/231-PowerOfTwo.py
@@ -0,0 +1,8 @@
+# Last updated: 04/02/2026, 22:44:49
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n //= 2
+        return n == 1
